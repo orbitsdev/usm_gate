695abcebd7939c3490adb62213767aa2b9d8df8b
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-10-18 15:47:36 +0800

    wip

--- resources/views/components/live-wire-layout.blade.php
+++ resources/views/components/live-wire-layout.blade.php
@@ -184,7 +184,7 @@
                 <li>
                   <a href="{{ route('logs') }}" class="{{ request()->routeIs('logs') ? 'bg-gray-50 text-indigo-600 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold' }}">
                     <span class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border {{ request()->routeIs('logs') ? 'border-indigo-600' : '' }} text-[0.625rem] font-medium">H</span>
-                    <span class="truncate">Error Logs</span>
+                    <span class="truncate">Debug Api & Hardware</span>
                 </a>
                 
                 </li>
