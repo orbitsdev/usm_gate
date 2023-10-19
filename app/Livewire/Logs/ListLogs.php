66fcc5892c735eb267efee084136a5773089c507
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-10-19 14:01:16 +0800

    wip

--- app/Livewire/Logs/ListLogs.php
+++ app/Livewire/Logs/ListLogs.php
@@ -27,6 +27,9 @@ class ListLogs extends Component implements HasForms, HasTable
         return $table
             ->query(Log::query())
             ->columns([
+              TextColumn::make('card.id_number')
+                    ->numeric()
+                    ->sortable(),
               TextColumn::make('card_id')
                     ->numeric()
                     ->sortable(),
