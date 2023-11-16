f2d78618cab847182040397d75f55b53b8f30e38
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-11-16 13:40:51 +0800

    update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,16 +13,28 @@ class UserSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
+    {   
+        $adminUser = User::where('email', 'admin@gmail.com')->first();
+
+    // If admin user doesn't exist, create it
+    if (!$adminUser) {
         User::create([
-            'name'=> 'Admin User',
-            'email'=> 'admin@gmail.com',
-            'password'=> Hash::make('password')
+            'name' => 'Admin User',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password')
         ]);
+    }
+
+    // Check if developer user exists
+    $developerUser = User::where('email', 'developer@gmail.com')->first();
+
+    // If developer user doesn't exist, create it
+    if (!$developerUser) {
         User::create([
-            'name'=> 'Developer User',
-            'email'=> 'developer@gmail.com',
-            'password'=> Hash::make('password')
+            'name' => 'Developer User',
+            'email' => 'developer@gmail.com',
+            'password' => Hash::make('password')
         ]);
+    }
     }
 }
