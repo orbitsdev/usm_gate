c2dcb4347e47e6ad87981652d3a882039f2ed4ab
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-03-12 21:51:18 +0800

    wip

--- app/Http/Resources/DailyRecordResource.php
+++ app/Http/Resources/DailyRecordResource.php
@@ -15,22 +15,22 @@ class DailyRecordResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-           
+           'id'=> $this->id,
             'Account' => $this->when(optional($this->card)->account, function () {
                 return optional($this->card->account)->getFullName() ?? 'No Account Found';
             }, 'No Account Found'),
 
-           
+
             'Card ID' => optional($this->card)->id_number ?? 'No Card Found',
             'QR Number' => optional($this->card)->qr_number ?? 'No Qr Found',
 
-           
+
             'Account Type' => optional(optional($this->card)->account)->account_type ?? 'No Account Found',
 
-         
+
             'Time In' => $this->entry ? optional($this->created_at)->format('l-F d, Y h:i:s A') : 'None',
 
-            
+
             'Time Out' => ($this->entry && $this->exit) ? optional($this->updated_at)->format('l-F d, Y h:i:s A') : '-- No Exit --',
 
         ];
